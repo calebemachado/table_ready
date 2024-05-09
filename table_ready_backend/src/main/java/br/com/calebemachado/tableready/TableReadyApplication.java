b125507bc8b831c881246515832a729b2fae35eb
Calebe Machado <calebe.machado@ifood.com.br>
2024-05-09 15:32:11 -0300

    start project code with springboot, java21 and manven dep. management

---
+++ table_ready_backend/src/main/java/br/com/calebemachado/tableready/TableReadyApplication.java
@@ -0,0 +1,13 @@
+package br.com.calebemachado.tableready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableReadyApplication.class, args);
+	}
+
+}
---
+++ table_ready_backend/src/test/java/br/com/calebemachado/tableready/TableReadyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calebemachado.tableready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableReadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
