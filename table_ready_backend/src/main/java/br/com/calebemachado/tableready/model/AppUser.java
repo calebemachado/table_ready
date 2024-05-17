bf4039a95a7a19869f405d08255db32fc51a7848
Calebe Machado <calebe.machado@ifood.com.br>
2024-05-17 16:14:42 -0300

    Add persistence layer and model user and phone entities

--- table_ready_backend/src/main/java/br/com/calebemachado/tableready/model/AppUser.java
+++ table_ready_backend/src/main/java/br/com/calebemachado/tableready/model/AppUser.java
@@ -1,12 +1,30 @@
 package br.com.calebemachado.tableready.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
 import java.util.UUID;
 
+@Entity
 public class AppUser {
+
+    @Id
     private UUID id;
     private String name;
     private String surname;
+
+    @OneToOne
+    @JoinColumn(name = "phone_id")
     private Phone phone;
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public AppUser() {
+    }
 
     public AppUser(String name, String surname, Phone phone) {
         this.name = name;
--- table_ready_backend/src/main/java/br/com/calebemachado/tableready/model/Phone.java
+++ table_ready_backend/src/main/java/br/com/calebemachado/tableready/model/Phone.java
@@ -1,7 +1,40 @@
 package br.com.calebemachado.tableready.model;
 
-public record Phone(
-        Integer areaCode,
-        Integer number
-) {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class Phone {
+
+    @Id
+    private UUID id;
+    private Integer areaCode;
+    private Integer number;
+
+    public Phone() {
+
+    }
+
+    public Phone(Integer areaCode, Integer number) {
+        this.areaCode = areaCode;
+        this.number = number;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Integer getAreaCode() {
+        return areaCode;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
 }
